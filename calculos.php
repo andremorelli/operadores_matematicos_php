acca30bee3edd9342289e0c4de6c64789527e216
andremorelli <morelliduarte@yahoo.com.br>
2021-03-21 15:08:24 -0300

    operadores matematicos

---
+++ calculos.php
@@ -0,0 +1,22 @@
+<?php
+// Operadores Matemáticos 
+// Adição
+// Subtração 
+// Multiplicação 
+// Divisão 
+// %mod
+
+$n1 = 7;
+$n2 = 3;
+
+echo $n1 + $n2;
+echo "<br/>";
+echo $n1 - $n2;
+echo "<br/>";
+echo $n1 * $n2;
+echo "<br/>";
+echo $n1 / $n2;
+echo "<br/>";
+echo $n1 % $n2;
+
+?>
